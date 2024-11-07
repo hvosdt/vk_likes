dba6f7bb57b45d910e31a1eb4b358897dbacb8d9
hvosdt <hvosdt@gmail.com>
2024-11-07 14:19:59 +0500

    add vk auth

--- flask_app.py
+++ flask_app.py
@@ -1,5 +1,5 @@
 # import necessary packages
-from flask import Flask, render_template, request, redirect, url_for, flash, session
+from flask import Flask, render_template, request, redirect, url_for, flash, session, make_response, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.utils import secure_filename
 from flask_admin import Admin
@@ -13,6 +13,9 @@ from flask_login import LoginManager
 from flask_login import login_user, logout_user
 from flask_login import login_required
 
+import requests
+import json
+from uuid import uuid4
 # initialize the Flask app
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
@@ -93,8 +96,6 @@ def logout():
     logout_user()
     return redirect(url_for('login'))
 
-
-
 # define a route for the home page
 @app.route('/')
 def index():
@@ -106,11 +107,40 @@ def index():
 # define a route for the dashboard page
 @app.route('/dashboard')
 @login_required
-def dashboard():
-    
+def dashboard():    
     users = User.select()
     return render_template('dashboard.html', users=users)
-    
+
+@app.route('/callback', methods=['GET'])
+def callback():
+    group_id = 221262044
+    if request.method == 'GET':
+        params = request.args.to_dict()
+        
+        #with open('vk_code.txt', 'w') as file:
+        #    file.write(str(params))
+        payload = json.loads(params['payload'])
+        silent_token = payload['token']
+        uuid = payload['uuid']
+        service_token = config.SERVICE_TOKEN
+        data = {
+            'token': silent_token,
+            'access_token': service_token,
+            'uuid': uuid,
+            'v': '5.131'
+        }
+        url = 'https://api.vk.com/method/auth.exchangeSilentAuthToken'
+        res = requests.post(url, data=data).json()
+        with open('vk_id.json', 'w') as file:
+            json.dump(res, file, indent=4)
+        result = {
+            'status': 'Success'
+        }
+        return make_response(jsonify(result), 200)        
+        
+@app.route('/vk_auth')
+def vk_auth():    
+    return render_template('auth.html', APP_ID=config.CLIENT_ID, REDIRECT_URL=config.REDIRECT_URL, STATE=uuid4())
 
 if __name__ == '__main__':
     
