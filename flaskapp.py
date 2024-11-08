71c22169a92c0e000c9e92f079e24c6d6ce4bdc3
hvosdt <hvosdt@gmail.com>
2024-11-08 12:37:48 +0500

    f

--- flaskapp.py
+++ flaskapp.py
@@ -45,7 +45,8 @@ def callback():
         device_id = params['device_id']
         state = params['state']
         client_id = config.CLIENT_ID
-        
+        with open('state1.txt', 'w') as file:
+            file.write(str(session[state]))
         data = {
             'code': code,
             'client_id': client_id,
@@ -70,11 +71,13 @@ def vk_auth():
     code_verifier = base64.urlsafe_b64encode(os.urandom(40)).decode('utf-8')
     #code_verifier = re.sub('[^a-zA-Z0-9]+', '', code_verifier)
     
-    code_challenge = hashlib.sha256(code_verifier.encode('utf-8')).digest()
+    code_challenge = hashlib.sha256(code_verifier.encode('utf-8'))
     #code_challenge = base64.urlsafe_b64encode(code_challenge).decode('utf-8')
     #code_challenge = code_challenge.replace('=', '')
     
     state = uuid4()
+    with open('state.txt', 'w') as file:
+        file.write(str(state))
     session[str(state)] = code_verifier
     return render_template('vk_auth.html', APP_ID=config.CLIENT_ID, REDIRECT_URL=config.REDIRECT_URL, STATE=state, CODE_CHALLENGE=code_challenge)
 
